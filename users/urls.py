2df3f13cc5d20d67b60017607633687189ac3316
DivyansuVerma <130806575+Divyansuverma@users.noreply.github.com>
2025-10-14 17:58:52 +0530

    add contact url

--- users/urls.py
+++ users/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('me/', UserProfileView.as_view(), name='user-profile'),
-    path('users/contact/', contact_form_view, name='contact-form'),
+    path('contact/', contact_form_view, name='contact-form')
 ]
