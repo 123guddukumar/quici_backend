679e4f2574c061fe6b4fc435792c39ceb464b8b3
DivyansuVerma <130806575+Divyansuverma@users.noreply.github.com>
2025-11-02 16:15:21 +0530

    debug false

--- quici/settings.py
+++ quici/settings.py
@@ -9,7 +9,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7zhugr7hk7f%#o8@wf)*yb9$i$y*1z@cw%qxy9d!)$0%qs#8ap')
 DEBUG = False
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com","*",'quici-backend-1.onrender.com','quicki.net']
+ALLOWED_HOSTS = [".elasticbeanstalk.com","*",'quici-backend-1.onrender.com']
 
 
 INSTALLED_APPS = [
